2b5651aff430114fbf8e8f62591441fc7f6dde1c
kylejhargreaves <102608293+kylejhargreaves@users.noreply.github.com>
2025-03-08 19:42:03 +0000

    Add results table

--- LeaderboardResults/src/app/app.component.ts
+++ LeaderboardResults/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ResultsTableComponent } from './results-table/results-table.component';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    ResultsTableComponent
+  ],
+  template: `<app-results-table></app-results-table>`
 })
-export class AppComponent {
-  title = 'LeaderboardResults';
-}
+export class AppComponent { }
