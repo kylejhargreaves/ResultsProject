01ffeacccc3fdb21202215c50bdada394653e21a
kylejhargreaves <102608293+kylejhargreaves@users.noreply.github.com>
2025-03-09 12:59:04 +0000

    Imports need to move to app.config.ts

--- LeaderboardResults/src/app/app.component.ts
+++ LeaderboardResults/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterOutlet } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 import { ResultsTableComponent } from './results-table/results-table.component';
 
@@ -9,9 +6,6 @@ import { ResultsTableComponent } from './results-table/results-table.component';
   selector: 'app-root',
   standalone: true,
   imports: [
-    HttpClientModule,
-    
-    RouterOutlet,
     MatTableModule,
     ResultsTableComponent
   ],
@@ -19,26 +13,3 @@ import { ResultsTableComponent } from './results-table/results-table.component';
   styleUrl: 'app.component.scss'
 })
 export class AppComponent { }
-
-
-// import { Component } from '@angular/core';
-
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { MatTableModule } from '@angular/material/table';
-// //import { ResultsTableComponent } from './results-table/results-table.component';
-// import { TestTableComponent } from './test-table/test-table.component';
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [
-//     HttpClientModule,
-//     BrowserAnimationsModule,
-//     MatTableModule,
-//     //ResultsTableComponent
-//     TestTableComponent
-//   ],
-//   //template: `<results-table></results-table>`
-//   template: `<app-test-table></app-test-table>`
-// })
-// export class AppComponent { }
-
