fdd1104707b77cb3583fa7ac9dce128a3733a9c4
kylejhargreaves <102608293+kylejhargreaves@users.noreply.github.com>
2025-03-10 14:24:26 +0000

    Add sorting icon, fix links

--- LeaderboardResults/src/app/results-table/results-table.component.ts
+++ LeaderboardResults/src/app/results-table/results-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild} from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResultsDataService, PlayerStats } from '../services/results-data.service';
 import { MatCardModule } from '@angular/material/card';
@@ -25,40 +25,45 @@ export class ResultsTableComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['position', 'playerName', 'gamesPlayed', 'totalScore'];
   dataSource: MatTableDataSource<PlayerStats> = new MatTableDataSource();
   errorMessage: string | null = null;
-
+ 
   @ViewChild(MatSort) sort!: MatSort;
   constructor(private resultsDataService: ResultsDataService) { }
-   
-    ngOnInit(): void {
-      this.resultsDataService.getPlayerStats().subscribe({
-        next: (data: PlayerStats[]) => {
-          this.dataSource.data = data;
 
-          // Alllow view to be initialized before trying to sort
-          setTimeout(() => {
-            if (this.sort) {
-              this.dataSource.sort = this.sort;
-            } else {
-              console.warn('MatSort is still not available');
-            }
-          });
-        },
-        error: (err) => {
-          this.errorMessage = 'Service is unavailable.';
-        }
-      });
-    }
+  ngOnInit(): void {
+    this.resultsDataService.getPlayerStats().subscribe({
+      next: (data: PlayerStats[]) => {
+        this.dataSource.data = data;
+
+        // Alllow view to be initialized before trying to sort
+        setTimeout(() => {
+          if (this.sort) {
+            this.dataSource.sort = this.sort;
+          } else {
+            console.warn('MatSort is still not available');
+          }
+        });
+      },
+      error: (err) => {
+        this.errorMessage = 'Service is unavailable.';
+      }
+    });
+  }
 
 
   ngAfterViewInit() {
-    if (this.sort) {
-      this.dataSource.sort = this.sort;
-    }
+    setTimeout(() => {
+      if (this.sort) {
+        this.dataSource.sort = this.sort;
+        console.log("MatSort reassigned after view init:", this.sort);
+      } else {
+        console.warn("MatSort is not available.");
+      }
+    });
   }
-
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     // Trim whitespace and force lowercase to make the filter case-insensitive.
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
 }
